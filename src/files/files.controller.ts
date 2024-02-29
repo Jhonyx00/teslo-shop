e4332d65bbf7d8543818e8ca420f85dba9678a62
Jhonatan Reyes <jbrr.jxr@example.com>
2024-02-29 15:07:19 -0600

    fix environment variable

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -14,10 +14,14 @@ import { diskStorage } from 'multer';
 
 import { fileFilter, fileNamer } from './helpers';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('product/:imageName') //url
   findProductImage(@Res() res: Response, @Param('imageName') imageName) {
@@ -52,7 +56,7 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
 
     return { secureUrl };
   }
