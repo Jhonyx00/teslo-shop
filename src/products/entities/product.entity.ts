ede2e453336bacf2a54872f7d002699eb85480cd
Jhonatan Reyes <jbrr.jxr@example.com>
2024-06-05 16:40:03 -0600

    add api property

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,46 +9,92 @@ import {
 } from 'typeorm';
 import { ProductImage } from './';
 import { User } from '../../auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: '1865212c-5a1e-40cf-9861-4835e68d2413',
+    description: 'Product ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'T-shirt Teslo',
+    description: 'Product title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Product price',
+  })
   @Column('float', {
     //use float instead of numeric
     default: 0,
   })
   price: number;
 
+  @ApiProperty({
+    example: 'lorem ipsum',
+    description: 'Product description',
+    default: null,
+  })
   @Column({
     type: 'text',
     nullable: true,
   })
   desciption: string;
 
+  @ApiProperty({
+    example: 't_shirt_teslo',
+    description: 'Product SLUG - for SEO',
+    uniqueItems: true,
+  })
   @Column('text', { unique: true })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product stock',
+    default: 0,
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
+  @ApiProperty({
+    example: ['M', 'XL', 'XXL'],
+    description: 'Product sizes',
+  })
   @Column('text', { array: true })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'women',
+    description: 'Product gender',
+  })
   @Column('text')
   gender: string;
 
+  @ApiProperty({
+    example: ['shirt'],
+    description: 'Product tags',
+  })
   @Column('text', { array: true, default: [] })
   tags: string[];
 
+  @ApiProperty({
+    example: ['8765120-00-A_0_2000.jpg', '8765120-00-A_1.jpg'],
+    description: 'Product images',
+  })
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
