de556802d97cc5cadab015a18d52c57e548ec43e
Jhonatan Reyes <jbrr.jxr@example.com>
2024-06-08 12:40:46 -0600

    add handle connection

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,21 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessageWsGateway {
+export class MessageWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messageWsService: MessageWsService) {}
+
+  handleConnection(client: Socket) {
+    console.log('conected', client.id);
+  }
+  handleDisconnect(client: Socket) {
+    console.log('disconected', client.id);
+  }
 }
