c6f7f5960a46e8db2ed2a40966cf8b9e68bd6c41
Jhonatan Reyes <jbrr.jxr@example.com>
2024-06-08 13:29:25 -0600

    identify connected clients

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -13,9 +13,11 @@ export class MessageWsGateway
   constructor(private readonly messageWsService: MessageWsService) {}
 
   handleConnection(client: Socket) {
-    console.log('conected', client.id);
+    this.messageWsService.registerClient(client);
+    console.log({ conectados: this.messageWsService.getConnectedClients() });
   }
   handleDisconnect(client: Socket) {
-    console.log('disconected', client.id);
+    this.messageWsService.removeClient(client.id);
+    console.log({ conectados: this.messageWsService.getConnectedClients() });
   }
 }
