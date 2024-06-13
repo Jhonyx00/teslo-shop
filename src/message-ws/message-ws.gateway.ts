a56d4c6a0e00f58190deb132212a86e936093a72
Jhonatan Reyes <jbrr.jxr@example.com>
2024-06-13 16:21:37 -0600

    validate web socket jwt

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -8,15 +8,31 @@ import {
 import { MessageWsService } from './message-ws.service';
 import { Server, Socket } from 'socket.io';
 import { newMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer() wss: Server;
-  constructor(private readonly messageWsService: MessageWsService) {}
+  constructor(
+    private readonly messageWsService: MessageWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    console.log(payload);
+
     this.messageWsService.registerClient(client);
 
     this.wss.emit(
@@ -36,6 +52,27 @@ export class MessageWsGateway
     client: Socket,
     payload: newMessageDto,
   ) {
-    console.log(client.id, payload);
+    //emit message only to the initial client
+
+    // client.emit('messages-from-server', {
+    //   fullName: 'Soy yo',
+    //   payload: payload.message || 'no-message!!',
+    // });
+
+    //emit message to all clients except the initial client
+    // client.broadcast.emit('messages-from-server', {
+    //   fullName: 'Soy yo',
+    //   payload: payload.message || 'no-message!!',
+    // });
+
+    //emmot message to all clients
+    this.wss.emit('message-from-server', {
+      fullName: 'Soy yo',
+      message: payload.message || 'no-message!!',
+    });
+
+    //client.join("ventas")
+    //this.wss.to("venas").emit(...)
+    //emits message only to clients connected to "ventas"
   }
 }
